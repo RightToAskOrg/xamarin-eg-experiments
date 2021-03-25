4640ec275434c5cbc868ffa6041c72788084ce43
Vanessa Teague <vanessa@thinkingcybersecurity.com>
2021-03-25 16:58:45 +1100

    * .gitignore: Very basic start to get EG and Xamarin.Forms compiling   together.
    * HelloEGWorld2.sln:
    * App.xaml:
    * App.xaml.cs:
    * Main.cs:
    * MainPage.xaml:
    * Info.plist:
    * AssemblyInfo.cs:
    * MainPage.xaml.cs:
    * AppDelegate.cs:
    * HelloEGWorld2.csproj:
    * Entitlements.plist:
    * MainActivity.cs:
    * Default.png:
    * Default@2x.png:
    * HelloEGWorld2.iOS.csproj:
    * AssemblyInfo.cs:
    * AboutAssets.txt:
    * Default-568h@2x.png:
    * AssemblyInfo.cs:
    * Default-Portrait.png:
    * colors.xml:
    * styles.xml:
    * HelloEGWorld2.Android.csproj:
    * AboutResources.txt:
    * LaunchScreen.storyboard:
    * Default-Portrait@2x.png:
    * Resource.designer.cs:
    * icon.png:
    * AndroidManifest.xml:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.png:
    * icon.xml:
    * Icon87.png:
    * Icon76.png:
    * Icon60.png:
    * Icon58.png:
    * Icon40.png:
    * Icon29.png:
    * Icon20.png:
    * Icon80.png:
    * icon_round.xml:
    * Icon120.png:
    * Icon152.png:
    * Icon167.png:
    * Icon180.png:
    * Icon1024.png:
    * Contents.json:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:
    * launcher_foreground.png:

---
+++ HelloEGWorld2/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ElectionGuard;
+
+namespace HelloEGWorld2
+{
+    public partial class MainPage : ContentPage
+    {
+        // ulong[] rValue;
+        ulong[] rValue = new ulong[] { 0, 0 };
+        ElementModP r;
+
+        public MainPage()
+        {
+            // ulong[] rValue = new ulong[] { 0, 0 };
+            r = new ElementModP(rValue);
+
+            InitializeComponent();
+        }
+
+        int count = 0;
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button)sender).Text = $"You clicked {count} times.";
+        }
+
+
+        void EG_Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            ((Button)sender).Text = $"You clicked the EG button.";
+        }
+    }
+}
